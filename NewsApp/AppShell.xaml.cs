df8c770712bfeb63f420cbb4e6c9b8714a5fef4e
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-20 03:55:35 +0800

    Shell Based Navagation

--- NewsApp/AppShell.xaml.cs
+++ NewsApp/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace NewsApp
+﻿using NewsApp.View;
+
+namespace NewsApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(NewsDetailPage),typeof(NewsDetailPage));
         }
     }
 }
