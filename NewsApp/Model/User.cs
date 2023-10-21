74101d0f0f6ec5564b940454084b5cb089f04313
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-21 17:49:46 +0800

    Overview

--- NewsApp/Model/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NewsApp.Model;
-
-public class User
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-}
--- NewsApp/ViewModel/NewsViewModel.cs
+++ NewsApp/ViewModel/NewsViewModel.cs
@@ -7,23 +7,6 @@ namespace NewsApp.ViewModel;
 
 public partial class NewsViewModel:ObservableObject
 {
-    [ObservableProperty]
-     string username;
-
-    [RelayCommand]
-     void Login()
-     {
-         var user = new User()
-         {
-             Name = "Maruf",
-             Email = "fuga02@gmail.com",
-             Phone = "931544780"
-         };
-
-         Shell.Current.GoToAsync($"{nameof(NewsDetailPage)}",new Dictionary<string, object>
-         {
-             {"UserKey",user}
-         });
-     }
+    
 
 }
